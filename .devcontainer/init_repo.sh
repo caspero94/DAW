ec4c4ad15eb6ee62801d0180a5314b81ed7b800a
caspero94 <84985267+caspero94@users.noreply.github.com>
2024-10-01 22:39:04 +0200

    Update init_repo.sh

--- .devcontainer/init_repo.sh
+++ .devcontainer/init_repo.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 REPO_URL="https://github.com/caspero94/DAW.git"
 TEMP_DIR="temp_repo"
@@ -6,8 +6,8 @@ TEMP_DIR="temp_repo"
 # Clona el repositorio en la carpeta temporal
 git clone $REPO_URL $TEMP_DIR
 
-# Copia el contenido a la carpeta de trabajo, incluyendo archivos ocultos
-cp -r $TEMP_DIR/. ./
+# Usa rsync para mover el contenido a la carpeta de trabajo, excluyendo la carpeta .git
+rsync -a --exclude='.git' $TEMP_DIR/ ./
 
 # Elimina la carpeta temporal
 rm -rf $TEMP_DIR
